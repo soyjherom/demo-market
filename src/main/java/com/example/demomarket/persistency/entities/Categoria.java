c1295a0edb6dddf5c86b8c449fd3694ecf471341
soyjherom <jherom@tecnica.co.cr>
2023-03-20 22:21:16 -0600

    [ADD] Missing entities

---
+++ src/main/java/com/example/demomarket/persistency/entities/Categoria.java
@@ -0,0 +1,43 @@
+package com.example.demomarket.persistency.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id_categoria")
+    private Integer idCategoria;
+    private String descripcion;
+    private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
