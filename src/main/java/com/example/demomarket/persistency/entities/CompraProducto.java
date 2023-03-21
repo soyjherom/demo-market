b5d49aedd88d9dc511870fa0ba01ee8a1d97c036
soyjherom <jherom@tecnica.co.cr>
2023-03-20 22:08:29 -0600

    [ADD] Entity with compound primary key

---
+++ src/main/java/com/example/demomarket/persistency/entities/CompraProducto.java
@@ -0,0 +1,47 @@
+package com.example.demomarket.persistency.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="compras_productos")
+public class CompraProducto {
+    @EmbeddedId
+    private CompraProductoPK id;
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+
+    public CompraProductoPK getId() {
+        return id;
+    }
+
+    public void setId(CompraProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
